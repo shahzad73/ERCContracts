1573a5490c946a16584cb21195495541cd827837
Shahzad Aslam <sa@digishares.io>
2023-09-30 22:10:50 +0500

    Added spacebear contract and removed everything else

---
+++ src/Erc20.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract ERC20Token is ERC20, Ownable {
+
+	constructor(uint256 _initialSupply, string memory _name ) ERC20(_name, _name) {
+
+			_mint(msg.sender , _initialSupply);
+
+	}
+
+
+    function mint (address to, uint256 amount)		
+        external        
+		onlyOwner
+        returns (bool)
+    {
+		 super._mint(to, amount);
+		 return true;
+    }
+
+
+    function burn (address to, uint256 amount)
+		external     
+		onlyOwner   
+        returns (bool)
+    {
+		 super._burn(to, amount);
+		 return true;
+    }
+
+
+}
